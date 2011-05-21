09762dbf2342c7711a5dd2108a359e47ab337340
Sumedh Vivek Sakdeo <ssakdeo@cs.ucla.edu>
2011-05-20 18:35:51 -0700

    changes

--- src/cc/meta/meta.h
+++ src/cc/meta/meta.h
@@ -148,9 +148,9 @@ public:
 	//!< for the chunk based on what has been allocated so far.
 	off_t nextChunkOffset;
     //!< array of string which uniquely identify handler
-    std::vector optionalHandler;
+    std::vector<string> optional_handler;
 
-	MetaFattr(FileType t, fid_t id, int16_t n, const std::string& optional_handler = NULL):
+	MetaFattr(FileType t, fid_t id, int16_t n, const std::string& handler_key = NULL):
 		Meta(KFS_FATTR, id), type(t), 
 		numReplicas(n), chunkcount(0), filesize(-1),
 		nextChunkOffset(0)
@@ -160,20 +160,20 @@ public:
 		mtime = ctime = crtime;
 		if (type == KFS_DIR)
 			filesize = 0;
-        if (optional_handler != NULL)
-            this->optional_handler.push_back(optional_handler);
+        if (handler_key != NULL)
+            this->optional_handler.push_back(handler_key);
 	}
 
 	MetaFattr(FileType t, fid_t id, struct timeval mt,
 		struct timeval ct, struct timeval crt,
-		long long c, int16_t n, const std::string& optional_handler = NULL): Meta(KFS_FATTR, id),
+		long long c, int16_t n, const std::string& handler_key = NULL): Meta(KFS_FATTR, id),
 		type(t), numReplicas(n), mtime(mt), ctime(ct),
 		crtime(crt), chunkcount(c), filesize(-1), nextChunkOffset(0)
 	{ 
 		if (type == KFS_DIR)
 			filesize = 0;
-        if (optional_handler != NULL)
-            this->optional_handler.push_back(optional_handler);
+        if (handler_key != NULL)
+            this->optional_handler.push_back(handler_key);
 	}
 
 	MetaFattr(): Meta(KFS_FATTR, 0), type(KFS_NONE) { }
